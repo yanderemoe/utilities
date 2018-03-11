32474bdc91f972d5a16b615c7074513e96a54a9b
yanderemoe <yanderemoe@users.noreply.github.com>
2018-03-11 19:54:00 +0000

    Update virtManagerWithSSHPubKey.sh

--- virtManagerWithSSHPubKey.sh
+++ virtManagerWithSSHPubKey.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-eval `ssh-agent -s`
-ssh-add privatekey.key
-virt-manager -c qemu+ssh://root@172.28.184.1/system
+virt-manager -c "qemu+ssh://virtmgr@10.224.102.31/system?keyfile=sshPrivateKey" --no-fork
